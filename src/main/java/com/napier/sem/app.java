27ae8cffadefa392bab9987eaa145091abcf5fcf
DishanFA <40647522@live.napier.ac.uk>
2024-11-08 11:45:09 +0000

    Docker compose file

--- src/main/java/com/napier/sem/app.java
+++ src/main/java/com/napier/sem/app.java
@@ -40,7 +40,7 @@ public class app
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
