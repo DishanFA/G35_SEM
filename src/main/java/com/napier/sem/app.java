84bc74197201c119436eaa5dbbd58eb574d14ab0
sean clark <40660932@live.napier.ac.uk>
2024-11-22 10:07:18 +0000

    trying to fix mai.yml

--- src/main/java/com/napier/sem/app.java
+++ src/main/java/com/napier/sem/app.java
@@ -42,7 +42,7 @@ public class app
                 // Wait a bit for db to start
                 Thread.sleep(300);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
