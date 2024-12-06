584d06a4edef4370485ae770679591c07aa3a5b5
sean clark <40660932@live.napier.ac.uk>
2024-12-06 09:27:14 +0000

    connect as db

--- src/main/java/com/napier/sem/app.java
+++ src/main/java/com/napier/sem/app.java
@@ -43,7 +43,7 @@ public class app
                 // Wait a bit for db to start
                 Thread.sleep(3000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
